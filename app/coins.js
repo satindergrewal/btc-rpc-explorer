154d160709663d5510b0fafe9c48dd40b84f6ebb
Satinder Grewal <grewal@lavabit.com>
2020-09-12 18:26:44 +1200

    add chips coin to explorer's coin options

--- app/coins.js
+++ app/coins.js
@@ -1,9 +1,11 @@
 var btc = require("./coins/btc.js");
 var ltc = require("./coins/ltc.js");
+var chips = require("./coins/chips.js");
 
 module.exports = {
 	"BTC": btc,
 	"LTC": ltc,
+	"CHIPS": chips,
 
-	"coins":["BTC", "LTC"]
+	"coins":["BTC", "LTC", "CHIPS"]
 };
