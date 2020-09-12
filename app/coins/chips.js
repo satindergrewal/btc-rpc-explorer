684ab86b908b567a34489d419ab16b1aeefdbdd1
Satinder Grewal <grewal@lavabit.com>
2020-09-12 22:51:24 +1200

    enabled rpc browser to test

--- app/coins/chips.js
+++ app/coins/chips.js
@@ -53,14 +53,10 @@ module.exports = {
 	name:"Chips",
 	ticker:"CHIPS",
 	logoUrlsByNetwork:{
-		"main":"/img/logo/chips.png",
-		"test":"/img/logo/chips.png",
-		"regtest":"/img/logo/chips.png"
+		"main":"/img/logo/chips.png"
 	},
 	siteTitlesByNetwork: {
-		"main":"Chips Explorer",
-		"test":"Testnet Explorer",
-		"regtest":"Regtest Explorer"
+		"main":"Chips Explorer"
 	},
 	siteDescriptionHtml:"<b>BTC Explorer</b> is <a href='https://github.com/janoside/btc-rpc-explorer). If you run your own [Bitcoin Full Node](https://bitcoin.org/en/full-node), **BTC Explorer** can easily run alongside it, communicating via RPC calls. See the project [ReadMe](https://github.com/janoside/btc-rpc-explorer) for a list of features and instructions for running.",
 	nodeTitle:"Chips Full Node",
